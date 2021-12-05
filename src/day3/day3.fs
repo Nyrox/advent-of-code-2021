7f4e3c23bc31e25df1b894e5fffba42efed420e0
Nyrox <root@nyrox.dev>
2021-12-05 21:02:50 +0100

    Fix day 3

--- src/day3/day3.fs
+++ src/day3/day3.fs
@@ -5,7 +5,7 @@ type Input = char seq seq
 
 
 let parseInput (input: string): Input =
-    let lines = Array.toSeq (input.Split("\n"))
+    let lines = Array.toSeq (input.Split("\r\n"))
     Seq.map (Seq.cast) lines
 
 
@@ -19,7 +19,7 @@ let findMostCommonBit input =
         )
     ) (Seq.map (fun _ -> 0) (Seq.head input)) input
     |> Seq.map (fun i -> 
-        if i <= 0 then '0' else '1')
+        if i < 0 then '0' else '1')
 
 let parseBinary (input: char seq) =
     System.String.Concat input
@@ -31,21 +31,23 @@ let solve1 (input: Input) =
     |> (fun i -> i * (~~~i &&& (uint 0b111111111111)))
 
 
-let rec filter op mask (input: (char seq * char seq) seq) =
+let rec filter op (input: (char seq * char seq) seq) =
+    let mask = findMostCommonBit (Seq.map (fun (x, _) -> x) input)
     let remaining = Seq.filter (fun (x, _) ->
         op (Seq.head mask) (Seq.head x)) input
     if Seq.length remaining > 1 then
-        filter op (Seq.tail mask) (Seq.map (fun (x, og) -> (Seq.tail x, og)) remaining)
+        filter op 
+            (Seq.map (fun (x, og) -> (Seq.tail x, og))
+            remaining)
     else
         let (rest, og) = Seq.head remaining
         printfn "%A, %A" (Seq.toArray og, Seq.toArray rest) (Seq.length mask)
         og
 
 let solve2 (input: Input) =
-    let mask = findMostCommonBit input
     let (oxy, co2) = (
-        filter (=) mask (Seq.map (fun x -> x, x) input),
-        filter (<>) mask (Seq.map (fun x -> x, x) input))
+        filter (=) (Seq.map (fun x -> x, x) input),
+        filter (<>) (Seq.map (fun x -> x, x) input))
     let (oxy, co2) = (parseBinary oxy, parseBinary co2)
     printfn "%A" (oxy, co2)
     (oxy * co2)
